3e2042f0d6c7ac8ed1779479fdbadcb1c9e63ed7
orenzikun722 <164862462+orenzikun722@users.noreply.github.com>
2026-06-10 22:13:27 +0900

    レイアウトが変わったためもう一度中身を読み取る機構の更新

--- readAndSave.py
+++ readAndSave.py
@@ -18,53 +18,73 @@ pdf_file_name = "menu-domitory-2026_2_henkou.pdf"
 
 
 
+
+
 def get_menu(target_date):
   with pdfplumber.open(pdf_file_name) as pdf:
     for page in pdf.pages:
-      table=page.extract_table()
+      table = page.extract_table()
       if not table:
         continue
-      headers=table[0]
+      headers = table[0]
       try:
-        date_col=headers.index(target_date)
+        date_col = headers.index(target_date)
       except ValueError:
         continue
-      breakfast = ""
-      lunch = ""
-      dinner = ""
-      for row in table:
-        try:
-          if row and row[0]=="朝\n食":
-            breakfast = row[date_col].split('ｴﾈﾙｷﾞｰ')[0].split('[Ａ]')[0].strip("\n")
-          elif row and row[0]=="昼\n食":
-            lunch = row[date_col].split('ｴﾈﾙｷﾞｰ')[0].split('[Ａ]')[0].strip("\n")
-          elif row and row[0]=="夕\n食":
-            dinner = row[date_col].split('ｴﾈﾙｷﾞｰ')[0].split('[Ａ]')[0].strip("\n")
-        except BaseException:
-          print(target_date + ": 取得に失敗しました")
-      if breakfast != "" and lunch != "" and dinner != "":
-        scopes = ["https://www.googleapis.com/auth/cloud-platform", "https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/spreadsheets"]
-        creds = Credentials.from_service_account_file(
-          "service_account.json",
-          scopes=scopes
-        )
-        client = gspread.authorize(creds)
-        sheet = client.open("寮食管理").sheet1
-        before_data = sheet.find(target_date)
-        if before_data == None:
-          sheet.append_row([target_date, breakfast, lunch, dinner])
-        else:
-          sheet.update_cell(before_data.row, before_data.col+1, breakfast)
-          sheet.update_cell(before_data.row, before_data.col+2, lunch)
-          sheet.update_cell(before_data.row, before_data.col+3, dinner)
-        print(target_date+"の記入完了")
-      else:
-        if breakfast == "":
-          print(target_date+": 朝食が取得できていません")
-        if lunch == "":
-          print(target_date+": 昼食が取得できていません")
-        if dinner == "":
-          print(target_date+": 夕食が取得できていません")
+      filtered_rows = []
+      for row_idx, row in enumerate(table):
+        if row_idx == 0:
+          continue
+        item = row[date_col]
+        if item is None:
+          continue
+        item = item.strip()
+        if item:
+          if "\nA\n" in item:
+            item = item.replace("\nA\n", "")
+            item = "[ Ａ ]\n" + item
+          else:
+            item = item.replace("A", "[ Ａ ]\n")
+          if "\nB\n" in item:
+            item = item.replace("\nB\n", "")
+            item = "[ Ｂ ]\n" + item
+          else:
+            item = item.replace("B", "[ Ｂ ]\n")
+            filtered_rows.append(item)
+        energy_indices = [
+          i for i, item in enumerate(filtered_rows) if "ｴﾈﾙｷﾞｰ" in item
+        ]
+        if len(energy_indices) >= 3:
+          breakfast_list = filtered_rows[0 : energy_indices[0]]
+          lunch_list = filtered_rows[energy_indices[0] + 1 : energy_indices[1]]
+          dinner_list = filtered_rows[energy_indices[1] + 1 : energy_indices[2]]
+          breakfast = "\n".join(breakfast_list)
+          lunch = "\n".join(lunch_list)
+          dinner = "\n".join(dinner_list)
+          if breakfast != "" and lunch != "" and dinner != "":
+            scopes = ["https://www.googleapis.com/auth/cloud-platform", "https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/spreadsheets"]
+            creds = Credentials.from_service_account_file(
+              "service_account.json",
+              scopes=scopes
+            )
+            client = gspread.authorize(creds)
+            sheet = client.open("寮食管理").sheet1
+            before_data = sheet.find(target_date)
+            if before_data == None:
+              sheet.append_row([target_date, breakfast, lunch, dinner])
+            else:
+              sheet.update_cell(before_data.row, before_data.col+1, breakfast)
+              sheet.update_cell(before_data.row, before_data.col+2, lunch)
+              sheet.update_cell(before_data.row, before_data.col+3, dinner)
+              print(target_date+"の記入完了")
+          else:
+            if breakfast == "":
+              print(target_date+": 朝食が取得できていません")
+            if lunch == "":
+              print(target_date+": 昼食が取得できていません")
+            if dinner == "":
+              print(target_date+": 夕食が取得できていません")
+
 
 
 
